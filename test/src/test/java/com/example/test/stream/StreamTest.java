0aafc45df4ba99482f8f6697ecf884c900f3ffaa
zl <zhaluobox@163.com>
2021-12-23 11:20:24 +0800

    Stream流验证测试

--- test/src/test/java/com/example/test/stream/StreamTest.java
+++ test/src/test/java/com/example/test/stream/StreamTest.java
@@ -11,13 +11,36 @@ public class StreamTest {
         var list = new ArrayList<String>();
 
         for (int i = 0; i < 10; i++) {
-            list.add(i + "");
+            list.add(String.valueOf(i));
         }
         var sorted = list.stream().mapToInt(Integer::valueOf).sorted();
         var reduce = sorted.filter(x -> x > 5).reduce(Integer::sum);
 
         if (reduce.isPresent()) {
             System.out.println(reduce.getAsInt());
+        }
+
+    }
+
+    @Test
+    public void midStreamTest() {
+
+        var list = new ArrayList<String>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(String.valueOf(i));
+        }
+        var sorted = list.stream().mapToInt(Integer::valueOf).sorted();
+
+        sorted.forEach(System.out::println);
+
+        System.out.println("第二次打印!============================");
+
+        try {
+            sorted.forEach(System.out::println);
+        } catch (IllegalStateException e) {
+            System.out.println("异常信息" + e.getMessage());
+            System.out.println("流一旦被归约节点使用,就无法再次使用");
         }
 
     }
