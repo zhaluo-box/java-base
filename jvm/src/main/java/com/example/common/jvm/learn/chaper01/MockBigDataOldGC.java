a5cb87e8e92c620165a97747e4ee7f407664d19a
zl <zhaluobox@163.com>
2021-12-24 17:25:30 +0800

    gclog

---
+++ jvm/src/main/java/com/example/common/jvm/learn/chaper01/MockBigDataOldGC.java
@@ -0,0 +1,22 @@
+package com.example.common.jvm.learn.chaper01;
+
+/**
+ * 模拟大对象进入老年代,触发full gc
+ * JDK 8
+ * -XX:NewSize=10485760 -XX:MaxNewSize=10485760 -XX:InitialHeapSize=20971520 -XX:MaxHeapSize=20971520 -XX:SurvivorRatio=8  -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=3145728 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc-mock-big-data.log
+ */
+public class MockBigDataOldGC {
+
+    public static void main(String[] args) {
+        byte[] array1 = new byte[4 * 1024 * 1024];
+        array1 = null;
+
+        byte[] array2 = new byte[2 * 1024 * 1024];
+        byte[] array3 = new byte[2 * 1024 * 1024];
+        byte[] array4 = new byte[2 * 1024 * 1024];
+
+        byte[] array5 = new byte[800 * 1024];
+
+        byte[] array6 = new byte[2 * 1024 * 1024];
+    }
+}
--- jvm/src/main/java/com/example/common/jvm/learn/chaper01/MockOldGC.java
+++ jvm/src/main/java/com/example/common/jvm/learn/chaper01/MockOldGC.java
@@ -9,16 +9,94 @@ package com.example.common.jvm.learn.chaper01;
  * 动态年龄判断15岁
  * JVM 参数
  * -XX:NewSize=10485760 -XX:MaxNewSize=10485760 -XX:InitialHeapSize=20971520 -XX:MaxHeapSize=20971520 -XX:SurvivorRatio=8  -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=10485760 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc-mock-old.log
+ * 大对象直接进入老年代
+ * -XX:NewSize=10485760 -XX:MaxNewSize=10485760 -XX:InitialHeapSize=20971520 -XX:MaxHeapSize=20971520 -XX:SurvivorRatio=8  -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=5242880 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc-mock-old.log
  */
 public class MockOldGC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        // 先分配8M 内存
+        //        test1();
+        //        testYear();
+        //        mockYearMoreThen15();
+        bigObjToOldGen();
+    }
+
+    /**
+     * 模拟大对象直接进入老年代
+     */
+    private static void bigObjToOldGen() {
+        byte[] array = new byte[7 * 1024 * 1024];
+    }
+
+    /**
+     * 模拟年龄超过15 进入老年代
+     * 新生代,总计10M内存,
+     * 8M + 2M + 128k
+     */
+    private static void mockYearMoreThen15() throws InterruptedException {
+
+        byte[] array2 = new byte[128 * 1024]; // 存活 分配128K数据 被持续引用 动态年龄判断,进入老年代
+
+        for (int i = 0; i < 20; i++) {
+            // 分配9M + 800K  释放
+            byte[] array1 = new byte[2 * 1024 * 1024];
+            array1 = new byte[2 * 1024 * 1024];
+            array1 = new byte[2 * 1024 * 1024];
+            array1 = new byte[2 * 1024 * 1024];
+            array1 = new byte[800 * 1024];
+            array1 = null; // 释放引用
+
+            byte[] array3 = new byte[128 * 1024];
+            array3 = null;
+
+            Thread.sleep(3000);
+            System.out.println("========" + i + "============");
+        }
+
+        while (true) {
+            Thread.sleep(30000);
+        }
+
+    }
+
+    /**
+     * 演示年龄
+     * 8:1:1
+     * 10
+     * 年龄1 + 年龄2 + 年龄N > S1区的50*  年龄N以上的直接进入老年代
+     */
+    private static void testYear() {
+        // 分配6M  释放
+        byte[] array1 = new byte[2 * 1024 * 1024];
+        array1 = new byte[2 * 1024 * 1024];
+        array1 = new byte[2 * 1024 * 1024];
+        array1 = null; // 释放引用
+
+        byte[] array2 = new byte[128 * 1024]; // 存活 分配128K数据 被持续引用 动态年龄判断,进入老年代
+
+        // 分配6m + 128K 释放
+        byte[] array3 = new byte[2 * 1024 * 1024]; // 一次垃圾回收
+
+        array3 = new byte[2 * 1024 * 1024];
+        array3 = new byte[2 * 1024 * 1024];
+        array3 = new byte[128 * 1024];
+
+        array3 = null; // 释放引用
+
+        byte[] array4 = new byte[2 * 1024 * 1024]; // 存活 第二次垃圾回收 存在于Eden区
+    }
+
+    /**
+     * 首次测试
+     */
+    private static void test1() {
+
+        // 先分配6M 内存
         byte[] array1 = new byte[2 * 1024 * 1024];
         array1 = new byte[2 * 1024 * 1024];
         array1 = new byte[2 * 1024 * 1024];
 
-        array1 = null;
+        array1 = null; // 释放引用
 
         byte[] array2 = new byte[128 * 1024];
         byte[] array3 = new byte[2 * 1024 * 1024];
