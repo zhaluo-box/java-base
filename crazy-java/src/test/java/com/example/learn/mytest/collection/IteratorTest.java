c814a5d7a46781c8bc2c879a220042597dd85cc1
zl <zhaluobox@163.com>
2022-05-24 09:12:00 +0800

    测试pdf 添加水印

--- crazy-java/src/test/java/com/example/learn/mytest/collection/IteratorTest.java
+++ crazy-java/src/test/java/com/example/learn/mytest/collection/IteratorTest.java
@@ -1,5 +1,8 @@
 package com.example.learn.mytest.collection;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -22,19 +25,150 @@ public class IteratorTest {
         return list;
     }
 
+    private List<Node> initNodes() {
+        List<Node> list = new ArrayList<>();
+        list.add(new Node("张三", ""));
+        list.add(new Node("李四", ""));
+        list.add(new Node("王五", ""));
+        list.add(new Node("赵六", ""));
+        return list;
+    }
+
     @Test
-    public void test() {
-        List<String> list = initList();
-        for (String str : list) {
-            //            try {
-            if (str.equals("王五")) {
-                list.remove("赵六");
+    public void test2() {
+        List<Node> nodes = initNodes();
+        for (Node node : nodes) {
+            Node exampleNode = new Node("王五", "");
+            if (exampleNode.equals(node)) {
+                nodes.remove(node);
             }
-            //            } catch (Exception e) {
-            //                // 预期抛出并发修改异常
-            //                e.printStackTrace();
-            //            }
+        }
+        System.out.println(nodes);
+    }
+
+    @Test
+    public void test3() {
+        List<String> list = new ArrayList<>();
+        list.add("001");
+        list.add("002");
+        list.add("003");
+
+        list.removeIf("003"::equals);
+    }
+
+    /**
+     * 并发修改异常测试：
+     * 移除 当前元素
+     * 结果，
+     * --张三
+     * --李四
+     * --王五
+     * --[张三, 李四, 赵六]
+     * =====》没有发生预期的并发修改异常
+     * 纯属巧合导致， 导致下次迭代器遍历的时候 hasNext 游标==容器的大小，没有调用 next 的检查，直接就结束的遍历
+     * 因为 if (王五） 刚好是倒数第二个元素，迭代器的游标 下次调用hasNext的时候刚刚好3 而list的size 也刚好是3
+     * cursor != size 刚好返回false. 所以next（） 获取元素中的检查modCount != expectedModCount 没有触发，导致没有抛出预期的异常
+     * public boolean hasNext() {
+     * return cursor != size;
+     * }
+     */
+    @Test
+    public void test4() {
+        List<String> list = new ArrayList<>();
+        list.add("张三");
+        list.add("李四");
+        list.add("王五");
+        list.add("赵六");
+        for (String str : list) {
+            System.out.println(str);
+            if ("王五".equals(str)) {
+                list.remove("王五");
+            }
         }
         System.out.println(list);
+    }
+
+    /**
+     * 并发修改异常测试：
+     * 移除 当前元素
+     * 结果，
+     * --张三
+     * --李四
+     * --王五
+     * --[张三, 李四, 赵六]
+     * =====》没有发生预期的并发修改异常
+     */
+    @Test
+    public void test4_1() {
+        List<String> list = new ArrayList<>();
+        list.add("张三");
+        list.add("李四");
+        list.add("王五");
+        list.add("赵六");
+        for (String str : list) {
+            System.out.println(str);
+            if ("李四".equals(str)) {
+                list.remove("王五");
+            }
+        }
+        System.out.println(list);
+    }
+
+    /**
+     * 并发修改异常测试：
+     * 移除 后面的元素
+     * --张三
+     * --李四
+     * --王五
+     * --[张三, 李四, 王五]
+     * =====》 也没有发生预期的并发修改异常
+     */
+    @Test
+    public void test5() {
+        List<String> list = new ArrayList<>();
+        list.add("张三");
+        list.add("李四");
+        list.add("王五");
+        list.add("赵六");
+        for (String str : list) {
+            System.out.println(str);
+            if ("王五".equals(str)) {
+                list.remove("赵六");
+            }
+        }
+        System.out.println(list);
+    }
+
+    /**
+     * 并发修改异常测试：
+     * 移除 前面的元素
+     * --张三
+     * --李四
+     * --java.util.ConcurrentModificationException
+     * ===》 发生了预期的并发修改异常
+     */
+    @Test
+    public void test6() {
+        List<String> list = new ArrayList<>();
+        list.add("张三");
+        list.add("李四");
+        list.add("王五");
+        list.add("赵六");
+        for (String str : list) {
+            System.out.println(str);
+            if ("李四".equals(str)) {
+                list.remove("王五");
+            }
+        }
+        System.out.println(list);
+    }
+
+    @Data
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    public static class Node {
+        private String name;
+
+        private String desc;
     }
 }
