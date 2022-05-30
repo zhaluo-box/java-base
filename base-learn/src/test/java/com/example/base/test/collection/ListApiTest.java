9e74717fd755ce6e1748ff496a93889e05e4b40b
zl <zhaluobox@163.com>
2022-05-30 17:15:26 +0800

    list api 测试

---
+++ base-learn/src/test/java/com/example/base/test/collection/ListApiTest.java
@@ -0,0 +1,69 @@
+package com.example.base.test.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created  on 2022/5/25 10:10:35
+ *
+ * @author zl
+ */
+public class ListApiTest {
+
+    /**
+     * list 交际测试
+     */
+    @Test
+    public void retainAllTest() {
+        // 创建一个动态数组
+        ArrayList<String> sites = new ArrayList<>();
+
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+
+        System.out.println("ArrayList 1: " + sites);
+
+        // 创建另一个动态数组
+        ArrayList<String> sites2 = new ArrayList<>();
+
+        // 往动态数组中添加元素
+        sites2.add("Wiki");
+        sites2.add("Runoob");
+        sites2.add("Google");
+        System.out.println("ArrayList 2: " + sites2);
+
+        // 保留元素
+        boolean b = sites.retainAll(sites2);
+        System.out.println(b);
+        System.out.println("保留的元素: " + sites);
+        boolean b1 = sites.retainAll(sites2);
+
+        System.out.println(b1);
+        System.out.println("保留的元素2: " + sites);
+    }
+
+    /**
+     * 空list foreach
+     */
+    @Test
+    @SuppressWarnings("all")
+    public void emptyListForeachTest() {
+        List<String> list = new ArrayList<>();
+        System.out.println(list.isEmpty());
+        list.forEach(System.out::println);
+    }
+
+    /**
+     * 空list foreach
+     * throw java.lang.NullPointerException: Cannot invoke "java.util.List.forEach(java.util.function.Consumer)" because "list" is null
+     */
+    @Test
+    @SuppressWarnings("all")
+    public void nullListForeachTest() {
+        List<String> list = null;
+        list.forEach(System.out::println);
+    }
+}
