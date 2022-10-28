c10e7760b877a84aa2534cf8443ec5e226fb20c6
zl <zhaluobox@163.com>
2022-10-28 11:36:21 +0800

    swtich break 与方法return， 循环 break,continue 测试

--- base-learn/src/test/java/com/example/base/test/syntax/SwitchTest.java
+++ base-learn/src/test/java/com/example/base/test/syntax/SwitchTest.java
@@ -21,7 +21,41 @@ public class SwitchTest {
         System.out.println("--------case B 之后会一直向下穿透直到遇到break 跳出-------");
     }
 
-    public void switchBreak(SwitchEnum type) {
+    @Test
+    @DisplayName("Switch break 与 method return 测试")
+    public void switchBreakAndMethodReturnTest() {
+        switchBreakAndMethodReturn(SwitchEnum.B, 3);
+    }
+
+    /**
+     * 测试结果
+     * B0
+     * B1
+     * B2
+     * 提前return method (这里B3 没有打印被提前continue;)
+     * B4
+     * B5
+     */
+    @Test
+    @DisplayName("Switch break 与 loop continue 测试")
+    public void switchBreakAndLoopContinueTest() {
+        switchBreakAndLoopContinue(3);
+    }
+
+    /**
+     * 结果：
+     * B0
+     * B1
+     * 提前 break loop;
+     * switch 跳出与循环跳出都是关键字break, 但是循环了起名称标记 可以采用 loopSign: for(){ break:loopSign } 的方式跳出循环
+     */
+    @Test
+    @DisplayName("switch break 与 loop break 测试")
+    public void switchBreakAndLoopBreakTest() {
+        switchBreakAndLoopBreak(2);
+    }
+
+    void switchBreak(SwitchEnum type) {
         switch (type) {
         case A:
             System.out.println("A");
@@ -35,7 +69,83 @@ public class SwitchTest {
         }
     }
 
-    public enum SwitchEnum {
+    void switchBreakAndMethodReturn(SwitchEnum type, int signal) {
+        switch (type) {
+        case A:
+            System.out.println("A");
+            break;
+        case B:
+            if (signal == 1) {
+                System.out.println("提前return method");
+                return;
+            }
+            System.out.println("B");
+            break;
+        case C:
+            System.out.println("C");
+            break;
+        default:
+            System.out.println("other");
+        }
+    }
+
+    /**
+     * switch break 与循环continue 测试
+     */
+    void switchBreakAndLoopContinue(int signal) {
+        SwitchEnum type = SwitchEnum.B;
+        if (signal > 5) {
+            throw new RuntimeException("signal 不能大于循环上限 5");
+        }
+        for (int i = 0; i <= 5; i++) {
+            switch (type) {
+            case A:
+                System.out.println("A" + i);
+                break;
+            case B:
+                if (i == signal) {
+                    System.out.println("提前return method");
+                    continue;
+                }
+                System.out.println("B" + i);
+                break;
+            case C:
+                System.out.println("C" + i);
+                break;
+            default:
+                System.out.println("other");
+            }
+        }
+    }
+
+    void switchBreakAndLoopBreak(int signal) {
+        SwitchEnum type = SwitchEnum.B;
+        if (signal > 5) {
+            throw new RuntimeException("signal 不能大于循环上限 5");
+        }
+        loop:
+        for (int i = 0; i <= 5; i++) {
+            switch (type) {
+            case A:
+                System.out.println("A" + i);
+                break;
+            case B:
+                if (i == signal) {
+                    System.out.println("提前 break loop;");
+                    break loop;
+                }
+                System.out.println("B" + i);
+                break;
+            case C:
+                System.out.println("C" + i);
+                break;
+            default:
+                System.out.println("other");
+            }
+        }
+    }
+
+    enum SwitchEnum {
         A, B, C
     }
 
