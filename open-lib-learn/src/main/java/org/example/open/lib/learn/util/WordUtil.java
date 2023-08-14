487413e3bda670225e3b4ea8ef0cad277f8e4ea9
zl <zhaluobox@163.com>
2023-08-14 14:28:37 +0800

    word 水印测试

---
+++ open-lib-learn/src/main/java/org/example/open/lib/learn/util/WordUtil.java
@@ -0,0 +1,55 @@
+package org.example.open.lib.learn.util;
+
+import com.spire.doc.Document;
+import com.spire.doc.FileFormat;
+import com.spire.doc.TextWatermark;
+import com.spire.doc.documents.WatermarkLayout;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * 基于Spire.Doc.Free 进行添加水印
+ * Created  on 2023/8/14 10:10:52
+ *
+ * @author zl
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class WordUtil {
+
+    /**
+     * 添加水印
+     */
+    public static void addWaterMark(URL url, OutputStream outputStream, String waterMark) throws IOException, FontFormatException {
+        var inputStream = url.openStream();
+        doAddWatermark(outputStream, waterMark, inputStream);
+    }
+
+    public static void addWaterMark(InputStream inputStream, OutputStream outputStream, String waterMark) throws IOException, FontFormatException {
+        doAddWatermark(outputStream, waterMark, inputStream);
+    }
+
+    private static void doAddWatermark(OutputStream outputStream, String waterMark, InputStream inputStream) throws FileNotFoundException {
+        try (inputStream) {
+            // Load the Word document
+            Document doc = new Document(inputStream);
+            TextWatermark textWatermark = new TextWatermark();
+            textWatermark.setText(waterMark);
+            textWatermark.setFontSize(16);
+            textWatermark.setLayout(WatermarkLayout.Diagonal);
+            textWatermark.setSemitransparent(true);
+            doc.setWatermark(textWatermark);
+            doc.saveToStream(outputStream, FileFormat.Docx);
+        } catch (Exception e) {
+            throw new RuntimeException("word 添加水印失败", e);
+        }
+    }
+
+}
+
---
+++ open-lib-learn/src/test/java/org/example/open/lib/learn/util/WordUtilTest.java
@@ -0,0 +1,29 @@
+package org.example.open.lib.learn.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created  on 2023/8/14 13:13:42
+ *
+ * @author zl
+ */
+class WordUtilTest {
+
+    @Test
+    @DisplayName("word 文件添加水印")
+    void addWaterMark() throws IOException, FontFormatException {
+        String docPath = "D:\\tmp-dir\\SD_201_客户主数据创建接口_技术说明_V1.0.docx";
+        String outPath = "D:\\tmp-dir\\SD_201_客户主数据创建接口_技术说明_V2.0.docx";
+
+        var fileInputStream = new FileInputStream(docPath);
+        var fileOutputStream = new FileOutputStream(outPath);
+
+        WordUtil.addWaterMark(fileInputStream, fileOutputStream, "那时，天下人的口音，言语都是一样的");
+    }
+}
