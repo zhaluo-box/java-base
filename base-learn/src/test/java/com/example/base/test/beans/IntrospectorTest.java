ef5561e1fcf68f289f844dad4ca87a9426ce0aa4
zl <zhaluobox@163.com>
2023-09-23 00:49:18 +0800

    no message

---
+++ base-learn/src/test/java/com/example/base/test/beans/IntrospectorTest.java
@@ -0,0 +1,11 @@
+package com.example.base.test.beans;
+
+/**
+ * TODO java.beans 包下面类的熟悉，最起码需要先混个脸熟
+ * Created  on 2023/9/23 00:0:09
+ *
+ * @author zl
+ * {@link java.beans.Introspector;}
+ */
+public class IntrospectorTest {
+}
